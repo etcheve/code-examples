6250b4b306c4e9c7cff982bca3faf2eebc0d61b3
etcheve <etcheve@gmail.com>
2025-11-23 22:25:06 +0000

    ps isolation

--- containers/cmd/main.go
+++ containers/cmd/main.go
@@ -26,7 +26,7 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	must(cmd.Run())
 }
@@ -42,7 +42,11 @@ func child() {
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
 	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Chroot("/home/etcheve/code-examples/containers/fake-fs"))
+	must(syscall.Chdir("/")) 
+	must(syscall.Mount("proc","proc","proc",0,""))
 	must(cmd.Run())
+	must(syscall.Unmount("proc",0))
 }
 
 func must(err error) {
