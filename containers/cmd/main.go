0a2ac170ca4896c7e3259264823fec6a99c032e1
etcheve <etcheve@gmail.com>
2025-11-23 21:31:47 +0000

    change hostname

--- containers/cmd/main.go
+++ containers/cmd/main.go
@@ -11,13 +11,27 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
 		panic("run is the only valid option for the moment")
 	}
 }
 
 func run() {
+	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	must(cmd.Run())
+}
+
+func child() {
 	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -27,6 +41,7 @@ func run() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+	must(syscall.Sethostname([]byte("container")))
 	must(cmd.Run())
 }
 
