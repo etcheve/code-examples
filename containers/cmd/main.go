c1752b27b4a64bb55deb3e66609dfbe1eecf87f0
etcheve <etcheve@gmail.com>
2025-11-23 20:02:33 +0000

    adding linting

--- containers/cmd/main.go
+++ containers/cmd/main.go
@@ -1,34 +1,36 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
 
-
-func main(){
-  switch os.Args[1]{
-  case "run":
-	run()
-  }
-  default:
-	panic("run is the only valid option for the moment")
+	default:
+		panic("run is the only valid option for the moment")
+	}
 }
 
-
-fund run (){
+func run() {
 	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
-	
-	cmd := exec.Command()
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
 	must(cmd.Run())
 }
 
-func must (err error){
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
