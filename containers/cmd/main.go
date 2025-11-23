2bdc5b1eb70df4f3dc3411dc8340b72d737f75f2
etcheve <etcheve@gmail.com>
2025-11-23 23:58:34 +0000

    pid limitation, using cgroup v2

--- containers/cmd/main.go
+++ containers/cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"strconv"
+	"path/filepath"
 )
 
 func main() {
@@ -33,7 +35,7 @@ func run() {
 
 func child() {
 	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
-
+	cg()
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -47,6 +49,27 @@ func child() {
 	must(syscall.Mount("proc","proc","proc",0,""))
 	must(cmd.Run())
 	must(syscall.Unmount("proc",0))
+}
+
+
+func cg() {
+    base := "/sys/fs/cgroup"
+    path := filepath.Join(base, "test_container_go")
+
+    // create the cgroup directory
+    if err := os.Mkdir(path, 0755); err != nil && !os.IsExist(err) {
+    	panic(err)
+    }
+    // enable pids controller in parent
+    // write "+pids" into cgroup.subtree_control
+    must(os.WriteFile(filepath.Join(base, "cgroup.subtree_control"), []byte("+pids"), 0o644))
+
+    // now you can set pids.max
+    must(os.WriteFile(filepath.Join(path, "pids.max"), []byte("10"), 0o644))
+
+    // add current process to this cgroup
+    pid := strconv.Itoa(os.Getpid())
+    must(os.WriteFile(filepath.Join(path, "cgroup.procs"), []byte(pid), 0o644))
 }
 
 func must(err error) {
